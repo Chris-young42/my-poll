0772a3b043c1bc941d22a8fb2aca9f9d3a20bd48
Chris-young42 <3101940893@qq.com>
2025-06-12 20:27:57 +0800

    backend2

--- backend/controllers/pollController.js
+++ backend/controllers/pollController.js
@@ -4,7 +4,7 @@ const Poll = require('../models/Poll');
 
 
 exports.createPoll = async (req, res) => {
-    
+
     const { question, type, options, creatorId } = req.body;
 
     if (!question || !type || !creatorId) {
@@ -20,9 +20,22 @@ exports.createPoll = async (req, res) => {
                 }
                 processedOptions = options.map(option => ({ optionText: option }));
                 break;
+            case "rating":
+                processedOptions = [1, 2, 3, 4, 5].map((value) => ({ optionText: value.toString() }))
+                break;
+            case "yes/no":
+                processedOptions = ["yes", "no"].map((option) => ({ optionText: option }))
+                break;
             case "open-ended":
                 processedOptions = [];
-                break
+                break;
+            case "image-based":
+                if (!options || options.length < 2) {
+                    return res.status(400).json({ message: "Image-based poll requires exactly two option" });
+                }
+                processedOptions = options.map(url => ({ optionText: url }));
+                break;
+
             default:
                 return res.status(400).json({ message: "Invalid poll type" });
         }
@@ -30,5 +43,134 @@ exports.createPoll = async (req, res) => {
         res.status(201).json({ poll: newPoll });
     } catch (error) {
         res.status(500).json({ message: "Error creating poll", error: error.message });
+    }
+}
+
+exports.getAllPolls = async (req, res) => {
+    const { type, creatorId, page = 1, limit = 10 } = req.query;
+    const filter = {}
+    const userId = req.user._id;
+    if (type) { filter.type = type }
+    if (creatorId) { filter.creator = creatorId }
+    try {
+        const pageNumber = parseInt(page, 10);
+        const pageSize = parseInt(limit, 10);
+        const skip = (pageNumber - 1) * pageSize;
+        const polls = await Poll.find(filter).populate('creator', "fullname username email profileImageUrl").populate({
+            path: "responses.voterId",
+            select: "username profileImageUrl fullname"
+        }).skip(skip).limit(pageSize).sort({ createdAt: -1 })
+
+        const updatedPolls = polls.map((poll) => {
+            const userHasVoted = poll.voters.some((voterId) =>
+                voterId.equals(userId)
+            )
+            return {
+                ...poll.toObject(),
+                userHasVoted
+            }
+        })
+
+        const totalPolls = await Poll.countDocuments(filter);
+        const stats = await Poll.aggregate([
+            {
+                $group: {
+                    _id: "$type",
+                    totalPolls: { $sum: 1 }
+                },
+            },
+            {
+                $project: {
+                    type: "$_id",
+                    count: 1,
+                    _id: 0,
+                }
+            }
+        ])
+
+        const allTypes = [
+            { type: "single-choice", lable: "Single Choice" },
+            { type: "rating", lable: "Rating" },
+            { type: "yes/no", lable: "Yes/No" },
+            { type: "image-based", lable: "Image Based" },
+            { type: "open-ended", lable: "Open Ended" }
+        ];
+        const statsWithDefaults = allTypes.map((pollType) => {
+            const stat = stats.find((item) => item.type === pollType.type);
+            return {
+                lable: pollType.lable,
+                type: pollType.type,
+                count: stat ? stat.count : 0,
+            }
+        }).sort((a, b) => b.count - a.count);
+        res.status(200).json({
+            polls: updatedPolls, 
+            currentPage: pageNumber,
+            totalPages: Math.ceil(totalPolls / pageSize),
+            totalPolls,
+            stats: statsWithDefaults
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Error getting polls", error: error.message });
+    }
+}
+
+exports.getVotedPoll = async (req, res) => {
+
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+exports.getPollById = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+exports.voteOnPoll = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+exports.closePoll = async (req, res) => {
+
+
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+
+exports.bookmarkPoll = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+exports.getBookmarkedPolls = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+exports.deletePoll = async (req, res) => {
+    try {
+
+    } catch (error) {
+
     }
 }
--- backend/routes/pollRoutes.js
+++ backend/routes/pollRoutes.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const { protect } = require('../middleware/authMiddleware')
-const {createPoll,}=require('../controllers/pollController')
+const { createPoll, getAllPolls, getVotedPoll, getPollById, voteOnPoll, closePoll, bookmarkPoll, getBookmarkedPolls, deletePoll } = require('../controllers/pollController')
 const router = express.Router();
-router.post('/create',protect,createPoll)
-module.exports = router;
+router.post('/create', protect, createPoll)
+router.get("/getAllPolls", protect, getAllPolls)
+router.get("/votedPolls", protect, getVotedPoll)
+router.get("/:id", protect, getPollById)
+router.post("/:id/vote", protect, voteOnPoll)
+router.post("/:id/close", protect, closePoll)
+router.post("/:id/bookmark", protect, bookmarkPoll)
+router.get("/user/bookmarked", protect, getBookmarkedPolls)
+router.delete("/delete/:id", protect, deletePoll)
+
+
+
+module.exports = router; 
